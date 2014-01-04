0f256ff24fb9b1a91c75e087a7e57b425f020176
Csaba Valyi <csaba.valy1@gmail.com>
2014-01-04 21:14:05 +0100

    selectfood and editfood fucntions added

--- app/scripts/controllers/foodrepo.js
+++ app/scripts/controllers/foodrepo.js
@@ -5,12 +5,20 @@ angular.module('firebasepracticeApp')
     /*global Firebase*/
     var FBURL='https://csabivalyi.firebaseio.com/';
     var foods = $firebase(new Firebase(FBURL));
-
+    $scope.foodToEdit ='';
+    $scope.showEditForm ='';
     // 3-way data binding between angular and firebase
     foods.$bind($scope,'foods').then(function(){
 
-      $scope.editFood = function(index, data){
-        $scope.foods[index] = data;
+      $scope.selectFood = function(index){
+        $scope.foodToEdit = index;
+        $scope.showEditForm = 'true';
+      };
+      $scope.editFood = function(index, name, price){
+        $scope.foods[index].name = name;
+        $scope.foods[index].price = price;
+        $scope.foodToEdit = '';
+        $scope.showEditForm = '';
       };
     });
   });

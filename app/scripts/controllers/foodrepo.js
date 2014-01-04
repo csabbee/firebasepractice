8e218602e6d108bda9624783df67dd042bf85872
Csaba Valyi <csaba.valy1@gmail.com>
2014-01-04 21:30:15 +0100

    select and edit food functions

--- app/scripts/controllers/foodrepo.js
+++ app/scripts/controllers/foodrepo.js
@@ -7,12 +7,15 @@ angular.module('firebasepracticeApp')
     var foods = $firebase(new Firebase(FBURL));
     $scope.foodToEdit ='';
     $scope.showEditForm ='';
+    $scope.editedFood = {};
     // 3-way data binding between angular and firebase
     foods.$bind($scope,'foods').then(function(){
 
       $scope.selectFood = function(index){
         $scope.foodToEdit = index;
         $scope.showEditForm = 'true';
+        $scope.editedFood.name = $scope.foods[index].name;
+        $scope.editedFood.price = $scope.foods[index].price;
       };
       $scope.editFood = function(index, name, price){
         $scope.foods[index].name = name;

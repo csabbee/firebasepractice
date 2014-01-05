806c07b559db94d2bd62d823357864a56072634f
Csaba Valyi <csaba.valy1@gmail.com>
2014-01-05 14:26:10 +0100

    foodmodal generated

---
+++ test/spec/directives/foodmodal.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Directive: foodmodal', function () {
+
+  // load the directive's module
+  beforeEach(module('firebasepracticeApp'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+});

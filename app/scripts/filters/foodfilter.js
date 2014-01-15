0e24cedfa2f5d408c9ec3cf38548a38910f3ef61
Csaba Valyi <csaba_valyi@epam.com>
2014-01-15 16:12:35 +0100

    writing to console removed

--- app/scripts/filters/foodfilter.js
+++ app/scripts/filters/foodfilter.js
@@ -8,7 +8,6 @@ angular.module('firebasepracticeApp')
 
     angular.forEach(input, function(data) {
       if (data.hasOwnProperty('name') && data.hasOwnProperty('price')) {
-        console.dir(data);
         result.push(data);
       }
     });

b0123d4bc5c8958a35ea3883ee214d0ab43c8d89
Csaba Valyi <csaba_valyi@epam.com>
2014-01-15 13:55:54 +0100

    JSLint formatting

--- app/scripts/directives/navbar.js
+++ app/scripts/directives/navbar.js
@@ -3,7 +3,7 @@
 angular.module('firebasepracticeApp')
   .directive('navbar', function () {
     return {
-      templateUrl: "views/templates/navbar.html", 
+      templateUrl: 'views/templates/navbar.html',
       restrict: 'E'
     };
   });
